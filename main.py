c81d0fb73764a1faf94517584e261b61dad5f3a4
Gabriel DB <119068235+CoininDev@users.noreply.github.com>
2023-04-22 21:47:01 -0300

    planejamento

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def get_playlist_id(json_playlist):
 
 def get_track_id(json_track):
     #TODO
+    pass
 
+def get_track_genre(json_track):
+    #TODO
+    pass
 #pesquisar todas as músicas da playlist
 playlist = sptf.search_playlist(token, "Emo Forever")
 playlist_id = get_playlist_id(playlist)
@@ -18,7 +22,15 @@ lógica:
     criar lista de grupos #dicionário[nome do gênero, array com ID das músicas];
     pra cada música:
         pesquisar o gênero;
-        se ainda não houver, criar um grupo com o nome do gênero
+        se ainda não houver, criar um grupo com o nome do gênero;
+        colocar o ID da música no grupo cujo o nome é igual ao gênero dela;
+    aleatorizar a ordem dos grupos;
+    criar uma playlist no spotify;
+    pra cada grupo:
+        pra cada música:
+            adicionar essa música à playlist.
+    pronto;
+    
 
 spotify_connect vai precisar:
     criar uma playlist;
